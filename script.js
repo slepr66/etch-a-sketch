2e20b7be1472a9aa7fe5444adabdfe38c70481f2
slepr66 <shinaniyash@gmail.com>
2025-04-10 11:38:10 +0500

    added hover sketch effect and button for dynamic grid size depending on user input

--- script.js
+++ script.js
@@ -12,8 +12,31 @@ function createGrid(num) {
             cell.style.border = "1px solid black";
             cell.style.flex = "1";
             row.appendChild(cell);
+
+            cell.addEventListener("mouseover", () => {
+                if (!cell.style.backgroundColor) {
+                    cell.style.backgroundColor = `hsl(${360 * Math.random()}, 50%, 50%)`;
+                } else if (cell.style.backgroundColor === "black") {
+                    cell.style.backgroundColor = "";
+                } else {
+                    cell.style.backgroundColor = "black";
+                };
+            });
         };
     };
 };
 
-createGrid(16)
+createGrid(16);
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    let gridSize;
+    
+    while (isNaN(gridSize) || gridSize > 100) {
+        gridSize = prompt("Please enter your desired grid dimensions number (0-100): ");
+    }
+    
+    container.replaceChildren();
+    createGrid(gridSize);
+});
